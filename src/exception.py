9d4d689b5464287109666b865fa1b813a73ede8e
asifjawad <jawad.asif@studenti.unipd.it>
2023-09-27 13:15:12 +0200

    Data ingestion

--- src/exception.py
+++ src/exception.py
@@ -19,10 +19,10 @@ def error_message_detail(error,error_detail:sys):
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message,error_detail=error_detail)
+        self.error_m = error_message_detail(error_message,error_detail=error_detail)
 
     def __str__(self):
-        return self.error_message
+        return self.error_m
 
 
 
@@ -32,5 +32,5 @@ if __name__ == "__main__":
     try:
         a=1/0
     except Exception as e:
-        logging.info("Logging has started")
+        logging.info("Zero Division Error")
         raise CustomException(e,sys)
