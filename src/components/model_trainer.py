b015a58b2fa59f6d3420400e0f52b9a8d64a75b1
asifjawad <jawad.asif@studenti.unipd.it>
2023-09-29 17:06:14 +0200

    Model Trainer

---
+++ src/components/model_trainer.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+from src.logger import logging
+from src.exception import CustomException
+
+from dataclasses import dataclass
+from src.utils import save_object
+
+@dataclass
+class 
