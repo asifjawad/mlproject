d86b7af8d56c6e31851ca54439f5641f2aa14513
asifjawad <jawad.asif@studenti.unipd.it>
2023-12-10 10:52:37 +0100

    Model Trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -3,8 +3,56 @@ import numpy as np
 from src.logger import logging
 from src.exception import CustomException
 
+import sys
+import os
+
 from dataclasses import dataclass
-from src.utils import save_object
+from src.utils import save_object, evaluate_model
+
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
 
 @dataclass
-class 
+class ModelTrainerConfig:
+    trained_model_file_path = os.join.path("artifacts", "model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_arr, test_arr):
+        logging.info("Model Trainer Initiated")
+        try:
+            train_x, train_y, test_x, test_y = (
+                train_arr[:,:-1],
+                train_arr[:,-1],
+                test_arr[:,:-1],
+                test_arr[:,-1]
+            )
+
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
+                # "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
+            }
+
+            evaluate_model(train_x, train_y, test_x, test_y, models)
+
+
+
+
+        except Exception as e:
+            CustomException(e,sys)
