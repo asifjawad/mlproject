3a4bc24dc5a5bd113a9996c36e08dfb27706f330
asifjawad <jawad.asif@studenti.unipd.it>
2023-12-11 20:19:37 +0100

    Predict Pipeline

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,23 @@
+import sys
+import pandas as pd
+from src.exception import CustomException
+
+from src.utils import load_object
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self,features):
+        
+
+
+class CustomData:
+    def __init__(self,
+                 )
+        
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict= {
+                 
+            }
